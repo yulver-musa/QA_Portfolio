d89bf4353f26b2a15cc4c2b9ff647a6d5662d418
Yulver <yulver.musa@gmail.com>
2025-08-30 18:54:18 +0200

    Edited the test file.

--- python_QA/tests/test_architect_hours.py
+++ python_QA/tests/test_architect_hours.py
@@ -22,12 +22,14 @@ def test_ah_01_name_John():
     assert is_valid_name(name) is True
     assert calculate_hours(1) == 3
 
+
 def test_ah_02_name_John_trailing():
     # Arrange
     name = "John "
     # Act & Assert
     assert is_valid_name(name) is True
     assert calculate_hours(1) == 3
+
 
 def test_ah_03_name_John_Anderson():
     # Arrange
@@ -36,12 +38,14 @@ def test_ah_03_name_John_Anderson():
     assert is_valid_name(name) is True
     assert calculate_hours(1) == 3
 
+
 def test_ah_04_name_John_Anderson_trailing():
     # Arrange
     name = "John Anderson "
     # Act & Assert
     assert is_valid_name(name) is True
     assert calculate_hours(1) == 3
+
 
 def test_ah_05_name_John_Anderson_leading():
     # Arrange
@@ -50,12 +54,14 @@ def test_ah_05_name_John_Anderson_leading():
     assert is_valid_name(name) is True
     assert calculate_hours(1) == 3
 
+
 def test_ah_06_name_John_Anderson_multiple():
     # Arrange
     name = "John   Anderson"
     # Act & Assert
     assert is_valid_name(name) is True
     assert calculate_hours(1) == 3
+
 
 # --Successful Count Test Cases--
 
