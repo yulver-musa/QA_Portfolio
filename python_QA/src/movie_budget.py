a1933dfce0c4e2e6e00edbe788a41f51e9f5063d
Yulver <yulver.musa@gmail.com>
2025-09-15 20:43:31 +0200

    Edit the new script for testing.

--- python_QA/src/movie_budget.py
+++ python_QA/src/movie_budget.py
@@ -17,8 +17,9 @@ def calculate_movie_budget(movie_budget, crew_members, costume_price, director_n
 
     """
 
-
     # --- Input validation ---
+
+
     if (
         not isinstance(movie_budget, (int, float)) or movie_budget <= 0 or
         not isinstance(crew_members, int) or crew_members <= 0 or
@@ -27,3 +28,13 @@ def calculate_movie_budget(movie_budget, crew_members, costume_price, director_n
         not isinstance(movie_name, str) or not movie_name.strip()
     ):
         return False, "Invalid input data provided.", None
+
+    # --- Business logic ---
+    decor = movie_budget * 0.1
+
+    if crew_members > 150:
+        costume_price *= 0.9
+
+    total_costumes = crew_members * costume_price
+    total_expenditures = total_costumes + decor
+
