936aae93dbe6de23a3751513e891c294cd9d46fa
Yulver <yulver.musa@gmail.com>
2025-09-16 21:05:27 +0200

    Updated the new file.

--- python_QA/tests/test_movie_budget.py
+++ python_QA/tests/test_movie_budget.py
@@ -5,3 +5,13 @@ from movie_budget import calculate_movie_budget
 
 def is_valid_budget(budget):
     return isinstance(budget, (int, float)) and budget > 0
+
+
+def is_valid_crew_members(crew):
+    return isinstance(crew, int) and crew > 0
+
+
+def is_valid_costume_price(costumes):
+    return isinstance(costumes, (int, float)) and costumes > 0
+
+
