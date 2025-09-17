c36e0dfb1ea19bb3fa4fd747748f2df06366c625
Yulver <yulver.musa@gmail.com>
2025-09-17 22:26:19 +0200

    Added new test case. Created first report.

--- python_QA/tests/test_movie_budget.py
+++ python_QA/tests/test_movie_budget.py
@@ -21,3 +21,14 @@ def is_valid_director_name(director):
 
 def is_valid_movie_name(movie):
     return isinstance(movie, str) and movie.strip() != ""
+
+
+# --Successful Budget Test Cases--
+
+
+def test_mb_01_integer_high():
+    # Arrange
+    budget = 500000
+    # Act
+    # Assert
+    assert is_valid_budget(budget) is True
