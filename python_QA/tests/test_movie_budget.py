42f5f665d30e7c6ee242f752f236777022fd5a59
Yulver <yulver.musa@gmail.com>
2025-10-09 18:23:29 +0200

    Updated test case.

--- python_QA/tests/test_movie_budget.py
+++ python_QA/tests/test_movie_budget.py
@@ -892,3 +892,7 @@ def test_mb_91_business_logic_unsuccessful():
     # Assert
     assert valid is False
     assert "Invalid input type. Please enter numbers where required."
+
+
+def test_mb_92_business_logic_unsuccessful():
+    # Arrange
