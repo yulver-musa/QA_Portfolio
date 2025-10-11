7f5371a33ee8045c3eed7f4f95f1d9835c7dc0e6
Yulver <yulver.musa@gmail.com>
2025-10-11 15:39:43 +0200

    Updated test case.

--- python_QA/tests/test_movie_budget.py
+++ python_QA/tests/test_movie_budget.py
@@ -1018,3 +1018,13 @@ def test_mb_99_business_logic_unsuccessful():
     # Assert
     assert valid is False
     assert "Invalid input type. Please enter numbers where required."
+
+
+def test_mb_100_business_logic_unsuccessful():
+    # Arrange
+    budget = -1000
+    crew_members = 0
+    costume_price = 0
+    director_name = ""
+    movie_name = ""
+    # Act
