328c0a0b431c2dcfe8aefb1a1ed29b73c046726f
Yulver <yulver.musa@gmail.com>
2025-11-10 20:11:41 +0200

    Updating Test Cases

--- python_QA/Playwright/test_saucedemo.py
+++ python_QA/Playwright/test_saucedemo.py
@@ -1,10 +1,18 @@
 from playwright.sync_api import Page
 import pytest
 
+#def test_title(page: Page):
+    #page.goto("https://www.saucedemo.com/")
+    #assert page.title() == "Swag Labs"
+
+#def test_inventory_site(page: Page):
+    #page.goto("https://www.saucedemo.com/inventory.html")
+    #assert page.inner_text('h3') == "Epic sadface: You can only access '/inventory.html' when you are logged in."
+
 def test_title(page: Page):
-    page.goto("https://www.saucedemo.com/")
+    page.goto("/")
     assert page.title() == "Swag Labs"
 
 def test_inventory_site(page: Page):
-    page.goto("https://www.saucedemo.com/inventory.html")
+    page.goto("/inventory.html")
     assert page.inner_text('h3') == "Epic sadface: You can only access '/inventory.html' when you are logged in."
