5d33077cf6460e67c2effb67d060cd75f3c4626a
Yulver <yulver.musa@gmail.com>
2025-11-10 20:08:02 +0200

    Updating Test Cases

--- python_QA/Playwright/test_saucedemo.py
+++ python_QA/Playwright/test_saucedemo.py
@@ -5,6 +5,6 @@ def test_title(page: Page):
     page.goto("https://www.saucedemo.com/")
     assert page.title() == "Swag Labs"
 
-def test_title(page: Page):
-    page.goto("https://www.saucedemo.com/")
-    assert page.title() == "Swag Labs"
+def test_inventory_site(page: Page):
+    page.goto("https://www.saucedemo.com/inventory.html")
+    assert page.inner_text('h3') == "Epic sadface: You can only access '/inventory.html' when you are logged in."
