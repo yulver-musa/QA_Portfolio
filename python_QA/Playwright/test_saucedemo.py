dd8b410974bb56e90ca9bbb28d0e410e11ba55bd
Yulver <yulver.musa@gmail.com>
2025-11-11 20:02:44 +0200

    Running the test with Chrome browser.

--- python_QA/Playwright/test_saucedemo.py
+++ python_QA/Playwright/test_saucedemo.py
@@ -10,7 +10,7 @@ import pytest
     #assert page.inner_text('h3') == "Epic sadface: You can only access '/inventory.html' when you are logged in."
 
 #Test run: pytest --headed --base-url https://www.saucedemo.com/ --browser chromium --browser firefox
-
+#Test run: pytest --headed --base-url https://www.saucedemo.com/ --browser-channel chrome
 #@pytest.mark.skip_browser("chromium")
 #@pytest.mark.only_browser("chromium")
 def test_title(page: Page):
