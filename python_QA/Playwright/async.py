27eddc7a7162315c1db8fcdc7e02a6426b1b2e8d
Yulver <yulver.musa@gmail.com>
2025-11-12 21:57:45 +0200

    Adding pytest.ini file.

--- python_QA/Playwright/async.py
+++ python_QA/Playwright/async.py
@@ -2,6 +2,11 @@ import asyncio
 from playwright.async_api import async_playwright
 
 async def main():
-    print("test")
+    async with async_playwright() as p:
+        browser = await p.firefox.launch(haadless=False)
+        page = await browser.new_page()
+        await page.goto("https://www.businesspromanagement.co.za/")
+        print(await page.title())
+        browser.close()
 
 asyncio.run(main())
